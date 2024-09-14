3cd4043a0eacf429d10f94711f3429ccf716db0b
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-09-14 12:05:10 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
         >
           <Image
             src="/images/logo.png"
-            width={120}
-            height={120}
+            width={110}
+            height={110}
             alt="Logo"
           />
         </motion.div>
