107c1c91d8a88a005e5fdf0c4ee815b6069ad9b5
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-09-18 02:29:16 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import Slider from "/src/components/Slider.jsx"
 import Stats from "/src/components/Stats.jsx"
+import TechStack from "/src/components/TechStack.jsx"
+import GraphicDesignStack from "/src/components/GraphicDesignStack.jsx"
+
 
 
 export default function Home() {
   return (
     <>
  <Slider/>
-    
   <Stats/>
+  <TechStack/>
+  <GraphicDesignStack/>
+ 
+ 
    
     </>
   );
