9c6aa3c7a423588bab2b6a9fa98f3dbb977b1273
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-09-21 11:06:17 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 import Navbar from '/src/components/Navbar.jsx'
 import Footer from '/src/components/Footer.jsx'
 
@@ -13,7 +14,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
+     <head>
+     <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9509QDW1XH"></Script>
+<Script id='google-analytics'>
+ { window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9509QDW1XH');
+ }
+</Script>
+     </head>
       <body className={inter.className}>
       <Navbar/>
         {children}
