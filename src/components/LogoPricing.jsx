d0138ad8a0127ff9efb33d91da07cf04e829c95e
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-09-26 21:08:27 -0700

    Update LogoPricing.jsx

--- src/components/LogoPricing.jsx
+++ src/components/LogoPricing.jsx
@@ -1,116 +1,106 @@
 'use client';
 
 import { useState } from 'react';
-import Link from 'next/link'
-
+import Link from 'next/link';
+import { FaPaintBrush, FaCrown, FaRocket } from 'react-icons/fa'; // Importing icons
+import { motion } from 'framer-motion'; // Framer Motion for animations
 
 const LogoPricing = () => {
   return (
     <section className="text-gray-400 bg-[#000319] body-font overflow-hidden">
       <div className="container px-5 pb-8 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
-          
-          <h2 className="text-4xl font-extrabold title-font -mb-6 mt-12 ">Logo Designing</h2>
-          
-      
+          <motion.h2 
+            className="text-4xl font-extrabold text-white title-font -mb-6 mt-12"
+            whileHover={{ scale: 1.1, rotate: 3 }} // Adding animation on hover
+            transition={{ type: 'spring', stiffness: 300 }}
+          >
+            🎨 Logo Designing Services
+          </motion.h2>
         </div>
         <div className="flex flex-wrap -m-4">
-          {/* Add your PricingCard components here with the updated code */}
+          {/* Pricing Cards */}
           <PricingCard
             title="Basic Logo Package"
             price="$49.99"
+        
             features={[
-             " 2 Custom Logo Design Concepts",
-"1 Dedicated Designer" ,            
-"3 Revisions" ,                           
-"All Final Files Format (AI, EPS, PNG, JPG)  " ,      
-"Complete Deployment",
-"Dedicated Accounts Manager",
-"100% Ownership Rights",
-"100% Satisfaction Guarantee",
-"100% Unique Design Guarantee",
-"100% Money Back Guarantee *"
+              "🎨 2 Custom Logo Design Concepts",
+              "👩‍🎨 1 Dedicated Designer",            
+              "🔁 3 Revisions",                           
+              "📁 All Final Files Format (AI, EPS, PNG, JPG)",      
+              "🚀 Complete Deployment",
+              "📝 Dedicated Accounts Manager",
+              "💼 100% Ownership Rights",
+              "😊 100% Satisfaction Guarantee",
+              "🛡 100% Unique Design Guarantee",
+              "💸 100% Money Back Guarantee *"
             ]}
             buttonText="Order Now"
             buttonStyle="bg-gray-800 hover:bg-gray-700"
-            footerText="Literally you probably haven't heard of them jean shorts."
           />
           <PricingCard
-              title="Start Up Logo Package"
-              price="$119.99"
-              features={[
-                "4 Custom Logo Design Concepts By 1 Designers (Industry specific)",
-                "FREE Icon" ,             
-                "FREE Business Card Design",
-                "UNLIMITED Revisions",
-                "All Final Files Format (AI, EPS, PNG, JPG, PDF)" ,   
-                "Complete Deployment",
-                "Dedicated Accounts Manager",
-                "100% Ownership Rights",
-                "100% Satisfaction Guarantee",
-                "100% Unique Design Guarantee"
+            title="Start Up Logo Package"
+            price="$119.99"
+         
+            features={[
+              "🚀 4 Custom Logo Design Concepts By 1 Designer",
+              "🌟 FREE Icon",             
+              "💳 FREE Business Card Design",
+              "🔁 UNLIMITED Revisions",
+              "📁 All Final Files Format (AI, EPS, PNG, JPG, PDF)",   
+              "🚀 Complete Deployment",
+              "📝 Dedicated Accounts Manager",
+              "💼 100% Ownership Rights",
+              "😊 100% Satisfaction Guarantee",
+              "🛡 100% Unique Design Guarantee"
             ]}
             buttonText="Order Now"
             buttonStyle="bg-gray-800 hover:bg-gray-700"
-            // footerText="Literally you probably haven't heard of them jean shorts."
           />
           <PricingCard
-              title="PROFESSIONAL LOGO Package"
-              price="$149"
-              features={[
-                "UNLIMITED Logo Design Concepts By 05 Designs",
-"UNLIMITED Revisions",
-"FREE Stationary Design Set",
-"FREE MS Word Letterhead",
-"All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)",
-"15% Discount on your next order" ,                         
-"Complete Deployment",
-"Dedicated Accounts Manager",
-"100% Ownership Rights",
-"100% Satisfaction Guarantee",
-"100% Unique Design Guarantee"
-            ]}
-            buttonText="Order Now"
-            buttonStyle="bg-gray-800 hover:bg-gray-700"
-            footerText="Literally you probably haven't heard of them jean shorts."
-          />
-   
-   
+            title="Professional Logo Package"
+            price="$149.99"
           
-          {/* Add other PricingCard components similarly */}
+            features={[
+              "👑 UNLIMITED Logo Design Concepts By 05 Designers",
+              "🔁 UNLIMITED Revisions",
+              "📂 FREE Stationary Design Set",
+              "📝 FREE MS Word Letterhead",
+              "📁 All Final Files Format (AI, PSD, EPS, PNG, GIF, JPG, PDF)",
+              "🎁 15% Discount on your next order",                         
+              "🚀 Complete Deployment",
+              "📝 Dedicated Accounts Manager",
+              "💼 100% Ownership Rights",
+              "😊 100% Satisfaction Guarantee",
+              "🛡 100% Unique Design Guarantee"
+            ]}
+            buttonText="Order Now"
+            buttonStyle="bg-gray-800 hover:bg-gray-700"
+          />
         </div>
       </div>
     </section>
   );
 };
 
-const PricingCard = ({ title, price, features, badge, buttonText, buttonStyle, footerText }) => {
+const PricingCard = ({ title, price, features, icon, buttonText, buttonStyle }) => {
   const [expanded, setExpanded] = useState(false);
   const toggleExpanded = () => setExpanded(!expanded);
 
   return (
-    <div className="p-4 md:w-1/2 w-full">
-    <div className={`h-full p-6 rounded-lg flex flex-col relative overflow-hidden ${badge ? "border-indigo-500" : "border-gray-700"}`}>
-      {badge && (
-        <span className="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">{badge}</span>
-      )}
-        <h2 className="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">{title}</h2>
-        <h1 className="text-5xl text-white pb-4 mb-4 border-b border-gray-800 leading-none">{price}</h1>
+    <motion.div
+      className="p-4 md:w-1/2 w-full hover:cursor-pointer"
+      whileHover={{ scale: 1.05 }} // Scale animation on hover
+    >
+      <div className={`h-full p-6 rounded-lg flex flex-col relative overflow-hidden ${icon ? "border-indigo-500" : "border-gray-700"}`}>
+        {icon && (
+          <div className="text-3xl mb-4">{icon}</div> // Displaying the icon
+        )}
+        <h2 className="lg:text-2xl sm:text-3xl sm:mb-2 tracking-widest text-gray-400 title-font mb-1 font-medium"> 🎨 {title}</h2>
+        <h1 className="lg:text-2xl sm:text-2xl text-white pb-4 mb-4 border-b border-gray-800 leading-none">💵 {price}</h1>
         {features.slice(0, expanded ? features.length : 5).map((feature, index) => (
           <p key={index} className="flex items-center text-gray-400 mb-2">
-            <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-800 text-gray-500 rounded-full flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2.5"
-                className="w-3 h-3"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 6L9 17l-5-5"></path>
-              </svg>
-            </span>
             {feature}
           </p>
         ))}
@@ -119,15 +109,20 @@ const PricingCard = ({ title, price, features, badge, buttonText, buttonStyle, f
             className="text-indigo-500 mt-3"
             onClick={toggleExpanded}
           >
-            {expanded ? 'Read Less' : 'Read More'}
+            {expanded ? 'Read Less' : 'Read More'} 📖
           </button>
         )}
-         <Link href="mailto:virtualcrafterstek@gmail.com" ><button className={`flex items-center mt-auto ${buttonStyle} border-0 py-2 px-4 w-full focus:outline-none  rounded-full`}>
-          {buttonText}
-        </button></Link>
-        {/* <p className="text-xs text-gray-400 mt-3">{footerText}</p> */}
+        <Link href="mailto:virtualcrafterstek@gmail.com">
+          <motion.button
+            className={`flex items-center mt-auto ${buttonStyle} border-0 py-2 px-4 w-full focus:outline-none rounded-full`}
+            whileHover={{ scale: 1.1 }} // Button hover effect
+            transition={{ type: 'spring', stiffness: 300 }}
+          >
+            {buttonText} ✉️
+          </motion.button>
+        </Link>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
